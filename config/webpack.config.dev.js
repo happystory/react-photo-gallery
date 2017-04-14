cef1d765d7ae2e23b9cbe6c6451fdacc1e537bbd
liuyao5201314 <670652018@qq.com>
2017-04-15 00:29:42 +0800

    support sass

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -113,7 +113,8 @@ module.exports = {
           /\.(js|jsx)(\?.*)?$/,
           /\.css$/,
           /\.json$/,
-          /\.svg$/
+          /\.svg$/,
+          /\.(scss|sass)$/
         ],
         loader: 'url',
         query: {
@@ -156,9 +157,13 @@ module.exports = {
         query: {
           name: 'static/media/[name].[hash:8].[ext]'
         }
-      }
+      },
       // ** STOP ** Are you adding a new loader?
       // Remember to add the new extension(s) to the "url" loader exclusion list.
+      {
+        test: /\.(scss|sass)$/,
+        loader: 'style!css!sass'
+      }
     ]
   },
   
